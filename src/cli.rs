bc8a6d4f312ff073bcf77ebcf074c862b12f7817
sakurai-ryo <sakurairyo.10do@gmail.com>
2022-09-29 12:23:13 +0900

    [add] add args struct

---
+++ src/cli.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Args {
+    /// Activate debug mode
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Command to execute inside the container
+    #[structopt(short, long)]
+    pub command: String,
+
+    /// User ID to create inside the container
+    #[structopt(short, long)]
+    pub uid: u32,
+
+    /// Directory to mount as root of the container
+    #[structopt(parse(from_os_str), short = "m", long = "mount")]
+    pub mount_dir: PathBuf,
+}
+
+pub fn parse_args() -> Args {
+    let args = Args::from_args();
+    args
+}
