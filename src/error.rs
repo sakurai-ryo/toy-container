328582306b52bbd25f0cf19bd962c79eb4eabbdc
sakurai-ryo <sakurairyo.10do@gmail.com>
2022-09-29 13:18:00 +0900

    [add] add error ded

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+use log::{debug, error};
+use std::fmt;
+use std::process::exit;
+
+#[derive(Debug)]
+pub enum Errcode {}
+
+impl Errcode {
+    pub fn get_retcode(&self) -> i32 {
+        1
+    }
+}
+
+#[allow(unreachable_patterns)]
+impl fmt::Display for Errcode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub fn exit_with_retcode(res: Result<(), Errcode>) {
+    match res {
+        Ok(_) => {
+            debug!("Exit without any error, returning 0");
+            exit(0);
+        }
+
+        Err(e) => {
+            let retcode = e.get_retcode();
+            error!("Error on exit:\n\t{}\n\tReturning {}", e, retcode);
+            exit(retcode);
+        }
+    }
+}
